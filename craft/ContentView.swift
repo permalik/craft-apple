55ef35dce495935c3e0aeb3b57d83e008a5fb79f
permalik <permalik@protonmail.com>
2025-07-07 22:02:02 -0500

    Initial Commit

---
+++ craft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  craft
+//
+//  Created by Ty Malik on 7/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ craft/craftApp.swift
@@ -0,0 +1,17 @@
+//
+//  craftApp.swift
+//  craft
+//
+//  Created by Ty Malik on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct craftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
